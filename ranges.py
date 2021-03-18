a24fb146b1e4f9bd9e0e4bc4aaea0747787cf0c8
Neal Ó Riain <neal.o.riain@gmail.com>
2021-03-18 13:15:01 +0000

    Update ranges.py

--- ranges.py
+++ ranges.py
@@ -1,15 +1,11 @@
 lst = [1,3,4,5,6,7,9]
 output = [(1,1), (3, 7), (9,9)]
 
-def is_left(left, val):
-    if not left + 1 == val:
+def not_consec(left, right):
+    if not left + 1 == right:
         return True
     return False
 
-def is_right(val, right):
-    if not right - 1 == val:
-        return True
-    return False
 
 def solve(lst):
     output = []
@@ -19,10 +15,10 @@ def solve(lst):
         left = lst[(i - 1) % len(lst)]
         rght = lst[(i + 1) % len(lst)]
 
-        if is_left(left, val):
+        if not_consec(left, val):
             bounds[0] = val
 
-        if is_right(val, rght):
+        if not_consec(val, rght):
             bounds[1] = val
             output.append(tuple(bounds))
 
